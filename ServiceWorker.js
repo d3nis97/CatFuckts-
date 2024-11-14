deb97bbc63621089a65dd334ed5729fb0b8b0a98
d3nis97 <denishepner@gmail.com>
2024-11-14 13:22:59 +0100

    Read URL parameter "token"

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Denis-CatFuckts!-1.0";
 const contentToCache = [
-    "Build/CatFuckts!.loader.js",
-    "Build/CatFuckts!.framework.js.unityweb",
-    "Build/CatFuckts!.data.unityweb",
-    "Build/CatFuckts!.wasm.unityweb",
+    "Build/TokenTest.loader.js",
+    "Build/TokenTest.framework.js.unityweb",
+    "Build/TokenTest.data.unityweb",
+    "Build/TokenTest.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
